f6c0241691e4fbf0557fcffe30aa8a0cf078337d
Yazdun <yazduntube@gmail.com>
2022-07-22 15:21:48 +0430

    commit

---
+++ app/root.jsx
@@ -0,0 +1,80 @@
+import {
+  LiveReload,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  Link,
+  Meta,
+} from "@remix-run/react";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
+
+export const meta = () => {
+  const description = "A cool blog built with Remix";
+  const keywords = "remix, react, javascript";
+
+  return {
+    description,
+    keywords,
+  };
+};
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href={globalStylesUrl} />
+        <title>{title ? title : "Remix Blog"}</title>
+      </head>
+
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container">{children}</div>
+    </>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  console.log(error);
+  return (
+    <Document>
+      <Layout>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  );
+}
---
+++ app/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Remix !</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum soluta
+        ex quia a doloribus, ratione deleniti distinctio reprehenderit! Quo
+        magnam aliquam consequuntur delectus eum impedit dolor molestiae optio!
+        Distinctio, neque.
+      </p>
+    </div>
+  );
+}
